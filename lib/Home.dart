4dfef1f941a7f68e506b7af260e538931988d105
jeremie <agbodjijeremie@gmail.com>
2023-12-22 09:16:48 +0000

    Workstation

--- lib/Home.dart
+++ lib/Home.dart
@@ -49,30 +49,33 @@ class HomeScreen extends StatelessWidget {
                       3, (index) =>
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Stack(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(20),
-                              height: screenWidth*0.5,
-                              width: screenWidth*0.5,
-                              decoration: BoxDecoration(
-                                color: index==0?Color(0xfff75e46):index==1?Color(0xfffcd983):Color(0xff3d1d2c),
-                                borderRadius:BorderRadius.all(Radius.circular(30)),
+                        child: ClipRRect(
+                          borderRadius:BorderRadius.all(Radius.circular(30)),
+                          child: Stack(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(20),
+                                height: screenWidth*0.5,
+                                width: screenWidth*0.5,
+                                decoration: BoxDecoration(
+                                  color: index==0?Color(0xfff75e46):index==1?Color(0xfffcd983):Color(0xff3d1d2c),
+                                  borderRadius:BorderRadius.all(Radius.circular(30)),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    index==0?Text("Documents",style: TextStyle(color: Colors.white,fontSize: screenWidth*0.05),):index==1?Text("Videos",style: TextStyle(color: Colors.white,fontSize: screenWidth*0.05),):Text("Images",style: TextStyle(color: Colors.white,fontSize: screenWidth*0.05),),
+                                    index==0?Text("230 MB",style: TextStyle(color: Colors.white54,fontSize: screenWidth*0.04),):index==1?Text("1.02 GB",style: TextStyle(color: Colors.white54,fontSize: screenWidth*0.04),):Text("3.0 GB",style: TextStyle(color: Colors.white54,fontSize: screenWidth*0.04),),
+                                  ],
+                                ),
                               ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  index==0?Text("Documents",style: TextStyle(color: Colors.white,fontSize: screenWidth*0.05),):index==1?Text("Videos",style: TextStyle(color: Colors.white,fontSize: screenWidth*0.05),):Text("Images",style: TextStyle(color: Colors.white,fontSize: screenWidth*0.05),),
-                                  index==0?Text("230 MB",style: TextStyle(color: Colors.white54,fontSize: screenWidth*0.04),):index==1?Text("1.02 GB",style: TextStyle(color: Colors.white54,fontSize: screenWidth*0.04),):Text("3.0 GB",style: TextStyle(color: Colors.white54,fontSize: screenWidth*0.04),),
-                                ],
+                              Positioned(
+                                  bottom: -40,
+                                  right: -40,
+                                  child: index==0?Image.asset('assets/folder.webp',height: screenWidth*0.4,):index==1?Image.asset('assets/video.png',height: screenWidth*0.4,):Image.asset('assets/gallery.webp',height: screenWidth*0.4,)
                               ),
-                            ),
-                            Positioned(
-                              bottom: -40,
-                                right: -40,
-                                child: index==0?Image.asset('assets/folder.webp',height: screenWidth*0.4,):index==1?Image.asset('assets/video.png',height: screenWidth*0.4,):Image.asset('assets/gallery.webp',height: screenWidth*0.4,)
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       )
                   ),
